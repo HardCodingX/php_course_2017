d9c7052b3044754357563f3720e4bd8d5b87fe59
Jesús Escamilla <jesusescamilla@hotmail.com>
2017-03-13 23:11:10 -0600

    more exersices for season 3

--- not_found.php
+++ not_found.php
@@ -1,4 +1,5 @@
 <?php
-// header("HTTP/1.0 404 Not Found");
-echo 'Hola';
+header("HTTP/1.0 404 Not Found");
+
+//echo 'Hola';
 ?>
--- session.php
+++ session.php
@@ -1,4 +1,18 @@
 <?php
+session_start();
+
+if (array_key_exists('contador',$_SESSION)) {
+  $_SESSION['contador']+=1;
+} else {
+  $_SESSION['contador']=1;
+}
+
+echo $_SESSION['contador'];
+
+if ($_SESSION['contador'] == 10) {
+  session_destroy();
+}
+
 
 
 ?>
